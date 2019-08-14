8d28ce62a6c1ad7a36dc94118db37d3b68fc526e
David Lohrentz <dwlohrentz@gmail.com>
2019-08-13 23:34:57 -0500

    updated views

--- drop_all.sql
+++ drop_all.sql
@@ -1,3 +1,3 @@
---DROP VIEW bkrs_percent_sum, cogs, quantity, formula;
+DROP VIEW bkrs_percent_sum, cogs, kamut_quant, formula;
 
 DROP TABLE customers, dough_ingredients, dough_instructions, preferments, dough_preferments, dough_shape_weights, doughs, ingredient_suppliers, ingredients, manufacturers, shapes, special_orders, suppliers, zip_codes;
